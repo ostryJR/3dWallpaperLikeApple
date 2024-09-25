3470dec6dcb97b6bcfe1fd50f24f7355627a3597
Mikolaj Ostrowski <mikolaj.p.ostrowski@gmail.com>
2024-09-25 14:06:30 +0200

    v2 working without animation

--- mainV2.py
+++ mainV2.py
@@ -14,9 +14,34 @@ clock = pg.time.Clock()
 
 # Set up the screen
 screen = pg.display.set_mode((cfg.WIDTH, cfg.HEIGHT))
-pg.display.set_caption(f"3D Landscape Wallpaper, set FPS:{cfg.targetFPS}, by GitHub @OstryJR")
+pg.display.set_caption(f"3D Landscape Wallpaper, set FPS:{cfg.targetFPS}, by GitHub @OstryJR, v2.0.0")
 
 
+def calculatePoints(func, scale):
+    result = []
+    for x in range(-cfg.WIDTH, cfg.WIDTH+1, 1):
+        result.append([x, cfg.functionFlat(x, func)])
+    
+    for idx in result: # setting the graph on the middle of the screen
+        idx[0] = idx[0]*10*scale + int(cfg.WIDTH/2)
+        idx[1] = (idx[1]*10 + int(cfg.HEIGHT/2))*scale + int(cfg.HEIGHT/3)*scale
+
+    result.append([cfg.WIDTH, -1])
+    result.append([cfg.WIDTH, cfg.HEIGHT])
+    result.append([-1, cfg.HEIGHT])
+    result.append([-1, -1])
+    
+    return result
+
+
+
+#make the functions
+functions = []
+for i in range(0, cfg.depth):
+    functions.append([rnd.random() for i in range(0,10)])
+    #functions.append([rnd.randint(0,10) for i in range(0,10)])
+
+print(functions)
 
 running = True
 while running:
@@ -34,13 +59,14 @@ while running:
     
     #calculate points
     calculatedPoints = []
-    
-    
+    for i in range(len(functions)-cfg.depth, len(functions)):
+        calculatedPoints.append(calculatePoints(functions[i], 1-(i*0.05)))
+    #print(calculatedPoints)
     
     #main code
     for idx, line in enumerate(reversed(calculatedPoints)):
-        #pg.draw.polygon(screen,cfg.colors[cfg.depth-idx-1].getColor(),line)
-        pg.draw.lines(screen,cfg.colors[cfg.depth-idx-1].getColor(),0,line)
+        pg.draw.polygon(screen,cfg.colors[cfg.depth-idx-1].getColor(),line)
+        #pg.draw.lines(screen,cfg.colors[cfg.depth-idx-1].getColor(),0,line)
    
     pg.display.flip()
 pg.quit()
